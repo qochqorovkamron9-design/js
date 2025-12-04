8123953c5562aff05691f8488a73b75a10461f51
qochqorovkamron9-design <qochqorovkamron9@gmail.com>
2025-12-04 15:15:18 +0500

    yangi

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
-let num = 2;
-let str = "Hello";
-const bool = true;
+// let num = 2;
+// let str = "Hello";
+// const bool = true;
 
 
-console.log(typeof num, typeof str, typeof bool);
+// console.log(typeof num, typeof str, typeof bool);
 
+let x = 10;
+let y = 20;
 
+if (x == y){
+    console.log("x is equal to y");
+}
+else if (x > y){
+    console.log("x is greater than y");
+}
+else {
+    console.log("x is less than y");
+}
